8b16ef00ce94897e5a574566ef039abc06351937
William Nolen <wpncomps@yahoo.com>
2020-10-23 04:07:10 -0400

    getters and setters of boid

--- Boids Visualization/src/Boid.java
+++ Boids Visualization/src/Boid.java
@@ -10,8 +10,54 @@ public class Boid {
 		this.coords = coordinates;
 		this.velocity.set(0, 0);
 		this.acceleration.set(0, 0);
-		float direction = 0;
+		this.direction = 0;
 		
 	}
+	
+	public PVector coords() {
+		return this.coords;
+	}
+	
+	public PVector velocity() {
+		return this.velocity;
+	}
+	
+	public PVector acceleration() {
+		return this.acceleration;
+	}
+	
+	public float direciton() {
+		return this.direction;
+	}
+	
+	public void setCoords(PVector c) {
+		this.coords.set(c);
+	}
+	
+	public void setVelocity(PVector v) {
+		this.velocity.set(v);
+	}
+	
+	public void setAcceleration(PVector a) {
+		this.acceleration.set(a);
+	}
+	
+	public void setDireciton(float d) {
+		this.direction = d;
+	}
+	
+	
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		String output = String.format("x: %f, y: %f, dx: %f, dy: %f, d2x: %f, d2y: %f", this.coords.x, this.coords.y, this.velocity.x, this.velocity.y, this.acceleration.x, this.acceleration.y);
+		
+		return output;
+		
+	}
+	
 	
 }
