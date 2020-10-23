66b1c285038297d71cde05a75769a7449c6064e4
William Nolen <wpncomps@yahoo.com>
2020-10-23 03:54:40 -0400

    populated constructor and instance variables of boid

--- Boids Visualization/src/Boid.java
+++ Boids Visualization/src/Boid.java
@@ -1,8 +1,17 @@
-import processing.core.PApplet;
-
-
-
+import processing.core.PVector;
 
 public class Boid {
 
+	PVector coords, velocity, acceleration;
+	float direction;
+	
+	public Boid(PVector coordinates) {
+		
+		this.coords = coordinates;
+		this.velocity.set(0, 0);
+		this.acceleration.set(0, 0);
+		float direction = 0;
+		
+	}
+	
 }
